54401cf0023ac1bafb0edf23baa4fdc01a56a0d6
Ldoun <67096173+Ldoun@users.noreply.github.com>
2023-03-21 13:07:44 +0900

    fix: n_layer to controll layer of model

--- call_tabular/model.py
+++ call_tabular/model.py
@@ -1,17 +1,29 @@
 import torch.nn as nn
 
 class DNN(nn.Module):
-    def __init__(self, input_size, hidden_size,output_size, drop_p, ) -> None:
+    def __init__(self, input_size, hidden_size,output_size, n_layer, drop_p,) -> None:
         super().__init__()
-        self.model = nn.Sequential(
-            nn.Linear(input_size, hidden_size),
+        input_layer = nn.Sequential(
+             nn.Linear(input_size, hidden_size),
             nn.ReLU(),
             nn.Dropout(drop_p),
-            nn.Linear(hidden_size, hidden_size),
-            nn.ReLU(),
-            nn.Dropout(drop_p),
-            nn.Linear(hidden_size, output_size),
         )
+
+        output_layer = nn.Linear(hidden_size, output_size),
+        
+
+        hidden_layers = []
+        for i in range(n_layer-2):
+            hidden_layers.append(
+                nn.Sequential(
+                    nn.Linear(hidden_size, hidden_size),
+                    nn.ReLU(),
+                    nn.Dropout(drop_p),
+                )
+            )
+
+        modules = [input_layer] + hidden_layers + [output_layer]
+        self.model = nn.Sequential(*modules)
 
     def forward(self, x):
         return self.model(x)
