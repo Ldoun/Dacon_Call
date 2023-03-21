5b03c56e6e39d4a9ec1973beaa332a0605f3ca4e
Ldoun <67096173+Ldoun@users.noreply.github.com>
2023-03-21 13:12:35 +0900

    fix: fix model

--- call_tabular/model.py
+++ call_tabular/model.py
@@ -3,18 +3,18 @@ import torch.nn as nn
 class DNN(nn.Module):
     def __init__(self, input_size, hidden_size,output_size, n_layer, drop_p,) -> None:
         super().__init__()
+        modules = []
+
+        
         input_layer = nn.Sequential(
              nn.Linear(input_size, hidden_size),
             nn.ReLU(),
             nn.Dropout(drop_p),
         )
+        modules.append(input_layer)
 
-        output_layer = nn.Linear(hidden_size, output_size),
-        
-
-        hidden_layers = []
         for i in range(n_layer-2):
-            hidden_layers.append(
+            modules.append(
                 nn.Sequential(
                     nn.Linear(hidden_size, hidden_size),
                     nn.ReLU(),
@@ -22,7 +22,9 @@ class DNN(nn.Module):
                 )
             )
 
-        modules = [input_layer] + hidden_layers + [output_layer]
+        output_layer = nn.Linear(hidden_size, output_size),
+        modules.append(output_layer)
+        
         self.model = nn.Sequential(*modules)
 
     def forward(self, x):
