973aec1174a8bad2ad920e43f756922df757f648
Ldoun <67096173+Ldoun@users.noreply.github.com>
2023-03-21 13:09:57 +0900

    fix: use n_layer

--- call_tabular/config.py
+++ call_tabular/config.py
@@ -7,7 +7,6 @@ def get_config():
     parser.add_argument("--raw_path", default= '../data')
 
     #train
-    parser.add_argument('--model', default='DNN')
     parser.add_argument('--seed', default=42, type=int)
     parser.add_argument('--n_fold', default=10, type=int)
     parser.add_argument('--learning_rate',default=1e-2, type=float)
@@ -16,7 +15,9 @@ def get_config():
     parser.add_argument('--batch_size', default=64, type=int)
 
     #model
+    parser.add_argument('--model', default='DNN')
     parser.add_argument('--hidden', default=16, type=int)
+    parser.add_argument('--n_layer', default=3, type=int)
     parser.add_argument('--drop_p', default=0.5, type=float)
 
     return parser.parse_args()
